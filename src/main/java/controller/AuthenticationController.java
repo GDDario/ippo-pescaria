cd2bd486e9d8f7984ba80ae0e4fa952b47d9df6d
GDDario <gd.dario.dev@gmail.com>
2024-10-18 00:52:13 -0300

    chore: add javax dependency at pom.xml

--- src/main/java/controller/AuthenticationController.java
+++ src/main/java/controller/AuthenticationController.java
@@ -4,12 +4,12 @@
  */
 package controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
